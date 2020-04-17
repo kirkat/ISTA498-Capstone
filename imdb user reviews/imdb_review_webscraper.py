069c621fcfd1cb07457f75f57d75188a9328d1d1
SkaSkeeter <42548634+SkaSkeeter@users.noreply.github.com>
2020-04-17 13:35:05 -0700

    Add files via upload

--- imdb user reviews/imdb_review_webscraper.py
+++ imdb user reviews/imdb_review_webscraper.py
@@ -1,9 +1,8 @@
 """
 ISTA 498
-This program creates two BeautifulSoup objects from the imdb Parasite user review "https://www.imdb.com/title/tt6751668/reviews?ref_=tt_urv"
-and imdb Avengers Endgame user review "https://www.imdb.com/title/tt4154796/reviews?ref_=tt_ov_rt".
-User reviews are parsed from the BeautifulSoup objects and used to create a DataFrame with pandas. The first 26 reviews are captured.
-Then the DataFrame is output to a 'parasite_imdb_review.csv' and imdb_avengers_review.csv' with 'User', 'Rating', 'Date', 'Title', 'Helpful', 'Comment' as the columns. 
+This program creates BeautifulSoup objects from the imdb Parasite user reviews and imdb Avengers Endgame user reviews.
+User reviews are parsed from the BeautifulSoup objects and used to create a DataFrame with pandas.
+Then the DataFrame is output to a 'parasite_imdb_review.csv' and imdb_avengers_review.csv' with 'User', 'Rating', 'Date', 'Title', 'Comment' as the columns. 
 """
 import requests
 import os.path
@@ -18,7 +17,7 @@ def get_soup(url):
 #This function creates a pandas dataframe and outputs the dataframe to a csv.
 def save_soup(fname, reviewees_info):
     df = pd.DataFrame(reviewees_info)
-    df.columns = ['User', 'Rating', 'Date', 'Title', 'Helpful', 'Comment']
+    df.columns = ['User', 'Rating', 'Date', 'Title', 'Comment']
     df.to_csv(fname, index=False)
 
 #This function creates a BeautifulSoup object and parses through each User review from an imdb user review.
@@ -44,11 +43,7 @@ def scrape_and_save(filename, url):
         
         #Title
         temp.append(review.find('a').contents[0])
-        
-        #Helpful
-        helpful = review.find('div', {'class' : 'actions text-muted'}).contents[0].strip('\n ')
-        temp.append(helpful)
-        
+       
         #Comment
         comment = review.find('div', {'class' : 'text'}).text
         temp.append(comment)
@@ -56,13 +51,38 @@ def scrape_and_save(filename, url):
         reviewees_info.append(temp)
         
     save_soup(filename, reviewees_info)
+    
+#This function takes CSV files with the same headers and combines them to one csv.
+def combine(files, new_fname):
+    storage = []
+    
+    for file in files:
+        df = pd.read_csv(file, index_col=None, header=0)
+        storage.append(df)
+        
+    frame = pd.concat(storage, axis=0, ignore_index=True)
+    frame.to_csv(new_fname, index=False)
 
 def main():
-    parasite_url = "https://www.imdb.com/title/tt6751668/reviews?ref_=tt_urv"
-    avengers_url = "https://www.imdb.com/title/tt4154796/reviews?ref_=tt_ov_rt"
     
-    scrape_and_save('imdb_parasite_review.csv', parasite_url)
-    scrape_and_save('imdb_avengers_review.csv', avengers_url)
+    parasite_url_1 = "https://www.imdb.com/title/tt6751668/reviews/_ajax"
+    parasite_url_2 = "https://www.imdb.com/title/tt6751668/reviews/_ajax?ref_=undefined&paginationKey=g4wp7cjnqa5tczyg7cxxxnrzrpsmmabhzfmxvlnomwklyczuf43o6ss6oi4vznjkd54k5v77ikp2u3tl54o5uy7zt23nzly"
+    parasite_url_3 = "https://www.imdb.com/title/tt6751668/reviews/_ajax?ref_=undefined&paginationKey=g4wp7cbeq43toyyf7otxvmjwrps4sbzhzfmxvlnomwklyczuf43o6ss6oqyfzmjjdn4k43xbrogmhwqosedhcghqbjirx6a"
+    
+    avengers_url_1 = "https://www.imdb.com/title/tt4154796/reviews/_ajax"
+    avengers_url_2 = "https://www.imdb.com/title/tt4154796/reviews/_ajax?ref_=undefined&paginationKey=g4wp7cjoryzdozyl7owxxmrzqpqm6bzhzfmxvlnomwklyczuf43o6ss7pazvlnzddv4k44uirvb5gs53wdzzuk26oybhwmq"
+    avengers_url_3 = "https://www.imdb.com/title/tt4154796/reviews/_ajax?ref_=undefined&paginationKey=g4wp7cjoqe3tayab66uhfmjzrhq42hjjtzpwzouokkd2gbzgpnt6ucksoy3vzmzob4d6h7spo5fmarq4grjfbx4jxt4v2"
+    
+    scrape_and_save('imdb_parasite_review_pt1.csv', parasite_url_1)
+    scrape_and_save('imdb_parasite_review_pt2.csv', parasite_url_2)
+    scrape_and_save('imdb_parasite_review_pt3.csv', parasite_url_3)
+    
+    scrape_and_save('imdb_avengers_review_pt1.csv', avengers_url_1)
+    scrape_and_save('imdb_avengers_review_pt2.csv', avengers_url_2)
+    scrape_and_save('imdb_avengers_review_pt3.csv', parasite_url_3)
+    
+    combine(['imdb_parasite_review_pt1.csv', 'imdb_parasite_review_pt2.csv', 'imdb_parasite_review_pt3.csv'], 'imdb_parasite_review.csv')
+    combine(['imdb_avengers_review_pt1.csv', 'imdb_avengers_review_pt2.csv', 'imdb_avengers_review_pt3.csv'], 'imdb_avengers_review.csv')
 
 if __name__ == "__main__":
     main()
